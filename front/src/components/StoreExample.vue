7cf90c69fbd004530e5c041577ca51962d1fe597
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2023-07-10 11:47:44 +0200

    front/feature-01
    feat: Added vue-js to the project for the front

---
+++ front/src/components/StoreExample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="text-h6">Voici un exemple d'utilisation d'un STORE PINIA</div>
+    <q-btn round color="secondary" @click="store.increment" icon="navigation" />
+    {{ store.counter }}
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from "../stores/counter";
+export default {
+  name: "StoreExample",
+  setup() {
+    const store = useCounterStore();
+    return {
+      store,
+    };
+  },
+};
+</script>
+<style></style>
